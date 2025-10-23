7cb3b258997497e5bea14f2c0d3b35a25aeef716
dudd44 <luciano.eduarda@escola.pr.gov.br>
2025-10-23 14:11:00 +0000

    aula02

--- App.js
+++ App.js
@@ -1,13 +1,13 @@
 import React from "react";
 import { ScrollView } from "react-native";
-import  Titulo from ".componente/Titulo";
+import  Titulo from "./componente/Titulo";
+import Saudacao from "./componente/Saldacao";
 
 export default function App(){
   return(
     <ScrollView style={{ padding:20 }}>
-      <Titulo>
-        
-      </Titulo>
+      <Titulo/>
+      <Saudacao nome="Duda"/>
 
 
     </ScrollView>
